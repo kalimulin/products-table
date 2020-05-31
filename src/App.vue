73de1bc9c8057a29c41c91e84d64fb9d2ed8a191
kalimulin <kalimulin-ru@yandex.ru>
2020-05-31 21:59:14 +0400

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <TableComponent msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import TableComponent from './components/TableComponent.vue'
+import {deleteProducts, getProducts} from './api/request'
+
+export default {
+  name: 'App',
+  components: {
+    TableComponent
+  },
+  methods: {
+    deleteHandler(id) {
+      deleteProducts(id).then(data => { console.log(data) }).catch((err) => {console.log(err)})
+    }
+  },
+  created() {
+    getProducts().then(data => { console.log(data) }).catch((err) => {console.log(err)})
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
